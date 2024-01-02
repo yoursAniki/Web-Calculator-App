0a99078d96ced7ebe1d0e3bebc6815e1b0e2f494
Danila <devdaniellle@gmail.com>
2024-01-02 20:35:37 +0500

    made html structure, css styles and js logic

---
+++ script.js
@@ -0,0 +1,66 @@
+// Обновить надпись на экране
+
+const updateScreen = () => {
+	screen.textContent = screenValue;
+};
+
+const screen = document.querySelector(".calculator__input-field");
+let screenValue = screen.textContent;
+
+// Удаление всех элементов экрана (Кнопка AC)
+
+const clearDisplay = () => {
+	screenValue = "0";
+	updateScreen();
+};
+
+// Написать число (Кнопки от 0 до 9)
+
+appendToDisplay = number => {
+	if (screenValue === "0") {
+		screenValue = number;
+		updateScreen();
+	} else {
+		screenValue += number;
+		updateScreen();
+	}
+};
+
+// Выбор операции (Кнопки деления, умножения, минус, плюс)
+
+let operation;
+let number1 = 0;
+let number2 = 0;
+
+const chooseOperation = op => {
+	operation = op;
+	number1 = parseInt(screenValue);
+	screenValue = "0";
+	updateScreen();
+};
+
+// Вычислить (Кнопка "=")
+
+const calculate = () => {
+	//todo прм продолжителном нажатии на '=' выполнять op
+	if (operation) {
+		number2 = parseInt(screenValue);
+		let result;
+		switch (operation) {
+			case "÷":
+				result = number1 / number2;
+				break;
+			case "×":
+				result = number1 * number2;
+				break;
+			case "-":
+				result = number1 - number2;
+				break;
+			case "+":
+				result = number1 + number2;
+				break;
+    }
+		screenValue = result;
+		updateScreen();
+	}
+};
